3130406597d8661685f7321de9d9052bf9bbfd71
ToF <cthibaut@octo.com>
2018-03-25 16:53:14 +0200

    Xavier + ToF

--- Minefield.hs
+++ Minefield.hs
@@ -1,19 +1,49 @@
 module Minefield
 where
-import Data.Char (digitToInt, intToDigit)
+import Data.Char (intToDigit)
 import Data.List (transpose)
 
-label ["*","."] = ["*","1"]
-label ss = labelHorz ss
+type Field = [String]
+type MappedField = [String]
+type MineMap = [[Int]]
+data Direction = North | South | West | East | NorthWest | SouthWest | NorthEast | SouthEast
+    deriving (Enum,Bounded,Show)
+
+mapField :: Field -> MappedField
+mapField grid = merge label (mineCount grid) grid
     where 
-    labelHorz = map labelRow
-    labelVert = transpose (map labelRow (transpose ss))
-    danger '*' '.' = '*'
-    danger '.' '.' = '0'
-    danger '.' '*' = '1'
-    labelLeft s  = zipWith danger s ((tail s)++".")
-    labelRight s = reverse (labelLeft (reverse s)) 
-    merge '*'  _  = '*'
-    merge  _  '*' = '*'
-    merge c d = intToDigit (digitToInt c + digitToInt d)
-    labelRow s = zipWith merge (labelLeft s) (labelRight s)
+    label :: Int -> Char -> Char
+    label _ '*' = '*'
+    label c '.' = intToDigit c
+
+merge f = zipWith (zipWith f)
+plus = merge (+)
+
+mineCount :: Field -> MineMap
+mineCount field = foldl1 plus (map (flip shift mineMapField) directions)
+    where
+    directions = (enumFromTo minBound maxBound)
+    mineMapField = mineMap field
+        
+left = tail . (++[0])
+right = reverse . left . reverse
+
+shiftHorz f = map f
+shiftVert f = transpose . map f . transpose 
+
+shift :: Direction -> MineMap -> MineMap
+shift North = shiftVert right
+shift South = shiftVert left
+shift West = shiftHorz left
+shift East = shiftHorz right
+shift NorthWest = shift North . shift West 
+shift SouthWest = shift South . shift West
+shift NorthEast = shift North . shift East
+shift SouthEast = shift South . shift East
+
+mineMap :: Field -> MineMap
+mineMap = map (map count)
+
+count :: Char -> Int
+count '.' = 0
+count '*' = 1 
--- Specs.hs
+++ Specs.hs
@@ -2,21 +2,52 @@ import Test.Hspec
 import Minefield
 
 main = hspec $ do
-    describe "label" $ do
+    describe "mapField" $ do
         it "given a 1x1 mine field, labels it with *" $ do
-            label ["*"] `shouldBe` ["*"]
+            mapField ["*"] `shouldBe` ["*"]
 
-        it "given a 1x1 free field, labels it with 0" $ do
-            label ["."] `shouldBe` ["0"]
+        it "given a 1x1 clear field, labels it with 0" $ do
+            mapField ["."] `shouldBe` ["0"]
 
-        it "given a 2x1 mine field at the end, labels it" $ do
-            label [".*"] `shouldBe` ["1*"]
+        it "given a 2x1 clear field, labels it with 00" $ do
+            mapField [".."] `shouldBe` ["00"]
 
-        it "given a 3x1 mine field at the end, labels it" $ do
-            label ["..*"] `shouldBe` ["01*"]
+        it "given a 2x1 mined field, labels it with **" $ do
+            mapField ["**"] `shouldBe` ["**"]
 
-        it "given a 2x1 mine field at the beginning, labels it" $ do
-            label ["*."] `shouldBe` ["*1"]
+        it "given a 2x1 field with 1 mine, labels it with 1*" $ do
+            mapField [".*"] `shouldBe` ["1*"]
 
-        it "given a 1x2 mine field, labels it" $ do
-            label ["*","."] `shouldBe` ["*","1"]
+        it "given a 3x1 field ending with 1 mine, labels it with 01*" $ do
+            mapField ["..*"] `shouldBe` ["01*"]
+
+        it "given a 3x1 field beginning with 1 mine, labels it with *10" $ do
+            mapField ["*.."] `shouldBe` ["*10"]
+
+        it "given a 2 rows clear field, labels it with 0,0" $ do
+            mapField [".","."] `shouldBe` ["0","0"]
+
+        it "given a 2 rows mine field, labels it with 0,1" $ do
+            mapField [".","*"] `shouldBe` ["1","*"]
+
+        it "given a 2x2 rows mine field, labels it" $ do
+            mapField ["*.",
+                      ".*"] `shouldBe` ["*2"
+                                       ,"2*"]
+
+        it "given a 2x2 rows mine field with mine south east, labels it" $ do
+            mapField [".."
+                     ,".*"] `shouldBe` ["11"
+                                       ,"1*"]
+
+        it "given a 2x2 rows mine field with mine north west, labels it" $ do
+            mapField ["*."
+                     ,".."] `shouldBe` ["*1"
+                                       ,"11"]
+
+        it "given the test case field, labels it" $ do
+            mapField ["*..."
+                     ,"..*."
+                     ,".*.."] `shouldBe` ["*211"
+                                         ,"23*1"
+                                         ,"1*21"]
