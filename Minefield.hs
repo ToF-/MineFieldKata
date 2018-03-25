9fdead305f17b4a74281b9aecbf0e60dbac66427
ToF <cthibaut@octo.com>
2018-03-25 11:44:57 +0200

    given a 2x1 mine field at the beginning, labels it -- general

--- Minefield.hs
+++ Minefield.hs
@@ -1,10 +1,15 @@
 module Minefield
 where
 
-label ["*."] = ["*1"]
-label [s] = [zipWith danger s ((tail s)++".")]
+label [s] = [zipWith merge (labelLeft s) (labelRight s)]
     where 
     danger '*' '.' = '*'
     danger '.' '.' = '0'
     danger '.' '*' = '1'
-
+    labelLeft s  = zipWith danger s ((tail s)++".")
+    labelRight s = reverse (labelLeft (reverse s)) 
+    merge '*'  _  = '*'
+    merge '0' '0' = '0'
+    merge '0' '1' = '1'
+    merge '1' '0' = '1'
+    
